dc08c6831af40fb1fa6ad4e3720b2a8853fb31ee
Marcelo Elias Del Valle <mvallebr@gmail.com>
2012-11-10 00:42:01 -0200

    changed library binary to the one compile with CMAKE_BUILD_TYPE=Release

--- c_src/ZorbaWrapperTest.cc
+++ c_src/ZorbaWrapperTest.cc
@@ -5,7 +5,7 @@ int
 main(int argc, char* argv[]){
 	int instance;
 	char *result;
-	char *transformation = "import module namespace jn = \"http://jsoniq.org/functions\"; \ndeclare variable $inputdoc external; \njn:parse-json($inputdoc)(\"satellites\")";
+	char *transformation = " \ndeclare variable $inputdoc external; \njn:parse-json($inputdoc)(\"satellites\")";
 	char *data ="{\"creator\" : \"Sat plugin\", \"satellites\" : {\"AAU CUBESAT\" : {\"tle1\" : \"1 27846U \", \"visible\" : false } } }";
 
 	printf("I will execute \n'%s'\n using \n'%s'\n", transformation, data);
